824ce61316106c25abefe946860da310417d1164
robert <robert@robert-ThinkPad-T450s>
2022-05-14 14:39:09 -0400

    the movement and sizes of the shape
    to learn

--- ParabolaTest.groovy
+++ ParabolaTest.groovy
@@ -1,9 +1,9 @@
 import eu.mihosoft.vrl.v3d.Parabola
 
-return [ Parabola.extrudeByEquation(5,0.27,0,1)
+return [ Parabola.extrudeByEquation(6,0.27,0,5)
 	.move(20,0,0),
-	Parabola.coneByEquation(5,1.27,0.7)
-	.move(30,0,0),
+	Parabola.coneByEquation(8,1.27,0.9)
+	.move(30,5,10),
 	Parabola.coneByFocalLength(10, 10),
 	Parabola.coneByHeight(10, 20)
 		.move(50,0,0)
